bc2e91fd39b5762e89341d44739f670910c83f4d
vishwitha nandaKumar <n.vishwitha@gmail.com>
2020-08-01 12:03:01 +0530

    handled topic exception

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from kafka.admin import NewTopic
+from kafka.errors import TopicAlreadyExistsError
+
 from config import Config
 from kafka import KafkaAdminClient
 
 conf = Config()
 admin_client = KafkaAdminClient(bootstrap_servers=conf.HOST_KAFKA, api_version=(1,0))
+
+
 topic_list = []
 topic_list.append(NewTopic(name=conf.TOPIC_NAME, num_partitions=conf.NO_OF_PARTITIONS, replication_factor=1))
-admin_client.create_topics(new_topics=topic_list, validate_only=False,)
+try:
+    admin_client.create_topics(new_topics=topic_list, validate_only=False,)
+except TopicAlreadyExistsError:
+    print("Topic {} already exists".format(conf.TOPIC_NAME))
