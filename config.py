c46cc16d55ad9aa0b76bf797ef8f0e46df892b55
vishwitha nandaKumar <n.vishwitha@gmail.com>
2020-08-02 14:17:06 +0530

    deployment yaml for kafka and zookeeper

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config(object):
     DEBUG = False
-    DB_NAME = "KAFKADB"
-    DB_HOST = "message_db"
+    DB_NAME = "messagedb"
+    DB_HOST = "mysql"
     DB_USERNAME = "root"
     DB_PASSWORD = "Mess123"
-    HOST_KAFKA ="my_kafka:9092"
+    HOST_KAFKA ="kafka:9092"
     TOPIC_NAME = "Item1"
     NO_OF_PARTITIONS = 2
-    GROUP_ID = "kafka_id8"
-    DB_TABLE = "kafka_table"
+    GROUP_ID = "my_consumer"
+    DB_TABLE = "message"
 
